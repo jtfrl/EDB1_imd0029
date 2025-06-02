198821d6637f9875089b26b06c3e8f7464938ed2
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-06-02 01:48:37 +0000

    add vect Del

---
+++ l2_aula6/vectDel.java
@@ -0,0 +1,33 @@
+public class vectDel{
+    public static int buscaBinaria(int[] vector, int k){
+        int inicio=0;
+        int fim=vector.length-1;
+        int count=0;
+          while(inicio<=fim){
+            count++;//contagem a cada interação
+                int meio=(inicio+fim)/2;
+                System.out.println("Busca #"+count+" | Eliminado o valor "+vector[meio]+" na busca de "+k+" no vetor.");
+                if(vector[meio]==k){
+                    System.out.println("Valor encontrado. Buscas feitas: "+count);
+                    return meio;
+                }else if(vector[meio]<k){
+                    inicio=meio+1;
+                }else{
+                    fim=meio-1;
+                }
+            }
+
+        System.out.println("Valor não encontrado. Buscas feitas: "+count);
+        return -1;
+    }
+
+    
+
+    public static void main(String[] args) {
+     int[] v={1,5,9,10,10,10,10,10,10,10,11,12,20,410,
+     419,464,464,464,883,1000,1100,1200,1300,1309,1320,
+     2629,4159,4160,4161,4162};
+     buscaBinaria(v, 464);
+    }
+
+}
