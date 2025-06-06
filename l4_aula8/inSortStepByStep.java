aaf8dc252c054c735aed353b41fab8707a6d4a1d
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-06-06 19:04:36 +0000

    add stepbystep (insort)

---
+++ l4_aula8/inSortStepByStep.java
@@ -0,0 +1,29 @@
+//q. 2 - lista 4
+
+import java.util.Arrays;
+
+public class inSortStepByStep{
+    public static void in_Sort(int[] vetor){
+        int tam=vetor.length;
+        int pas=1;
+        for(int i=1;i<tam;i++){ //análise a partir do 2o. índice
+            int k=vetor[i];
+            int j=i-1;
+            while(j>=0 && vetor[j]>k){
+                vetor[j+1]=vetor[j];
+                j--;
+                System.out.println("Passo: "+(pas++)+" | Vetor em ordenação: "+Arrays.toString(vetor));
+                System.out.println("\n");
+            }
+            vetor[j+1]=k; //troca para continuar laço
+        }
+        System.out.println("\n Vetor ordenado: ");
+    }
+
+
+    public static void main(String... args){
+        int[] v={18,9,2,11,20,38,47,45,56,36,-6,-12};
+        in_Sort(v); //tipo void: não é possível colocar na linha abaixo
+        System.out.println(Arrays.toString(v));
+    }
+}
