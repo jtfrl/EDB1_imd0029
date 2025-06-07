d3755e0480a1539db8edcc18d467e7ce4527a8d9
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-06-07 22:32:28 +0000

    add stepbystep tros

--- l5_aula9/bBTros.java
+++ l5_aula9/bBTros.java
@@ -1,24 +1,19 @@
-//q. 2 - lista 5
+//q. 3 - lista 5
 
 import java.util.Arrays;
 
-public class bBSortStepByStep{
-    public static void bbsort(int[] v){
+public class bBTros{
+    public static void bbtros(int[] v){
         int tam=v.length;
-        int pas=1;
         for(int i=0; i<v.length-1;i++){
             for(int j=0;j<v.length-1-i;j++){
-                if(v[j]>v[j+1]){
+                if(v[j]<v[j+1]){
                     int t=v[j];
                     v[j]=v[j+1];
                     v[j+1]=t;
-
-                    System.out.println("Passo: "+(pas++)+" | Vetor em ordenação: "+
-                    Arrays.toString(v));
                 }
             }
         }
-        System.out.println("\nVetor ordenado: \n");
     }
 
 
@@ -26,7 +21,7 @@ public class bBSortStepByStep{
     public static void main(String[] args){
         int[] n={-30, -7, 192,168,172,139,200,401,404,0,-2};
 
-        bbsort(n);
+        bbtros(n);
         System.out.println(Arrays.toString(n));
     }
 }
