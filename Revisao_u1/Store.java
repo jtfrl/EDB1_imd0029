963bd6eac1c549fd6965e45f259fde8c886173be
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-06-13 15:27:24 +0000

    add prob files not working with package

--- Revisao_u1/Store.java
+++ Revisao_u1/Store.java
@@ -1,32 +1,12 @@
+package src;
+
 //q. 6 (parte 1) - Revisão em 10.6.25
 
 public class Store{
-    private static Store[] prod=new prod[100];
-    private static String[] nome=new nome[100];
-    private static double[] preco=new preco[100];
+    private Store[] prod=new prod[100];
+    private String[] nome=new nome[100];
+    private double[] preco=new preco[100];
     private int prodCount=0;
-
-    /*
-    public Store(String nome, double preco){
-     this.nome=nome;
-     this.preco=preco;
-    }
-    public String getNome(){
-        return this.nome;
-    }
-
-    public double getPreco(){
-        return this.preco;
-    }
-
-    public void setNome(String nome){
-        nome.add(nome);
-    }
-
-    public void setPreco(double p){
-        preço=p;
-    }
-         */
 
     public void addProduct(String name, double price){
         if(prodCount<100){
@@ -34,8 +14,14 @@ public class Store{
             preco[prodCount]=preco;
             prodCount++;
             System.out.println("Adicionado: "+nome+" (R$ "+preco+")" );
+        }else{
+            System.out.println("Erro! Já há 100 produtos registrados. Não é possível adicionar.");
         }
     }
+
+    public void displayProd(){}
+
+
 
 //ver restante da implementacao no email
 }
--- Revisao_u1/pplStore.java
+++ Revisao_u1/pplStore.java
@@ -1,13 +1,22 @@
+package src;
+
 //q. 6 (parte 2) - Revisão em 10.6.25
 
 
-import Store;
+//import Store;
 
 public class pplStore{
-    
-    public static void main(String...  args){
-        for(int i=0; i<100;i++){
-
-        }
+ 
+      public static void main(String[] args) {
+        Store myStore = new Store();
+        
+        // Add sample products
+        myStore.addProduct("Notebook", 2500.00);
+        myStore.addProduct("Smartphone", 1200.50);
+        myStore.addProduct("Headphones", 199.90);
+        
+        // Display all products
+        //myStore.displayProducts();
     }
+   
 }
