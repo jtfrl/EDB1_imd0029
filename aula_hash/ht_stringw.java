390d2808c2e3da2e0df1ade45adde41caa77fe1a
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-07-15 16:55:06 +0000

    add ht string with weight

---
+++ aula_hash/ht_stringw.java
@@ -0,0 +1,30 @@
+//aula - 15.7
+//criar uma hash para stirngs com pesos
+
+
+public class No{
+    int v;
+    No prox=null;
+
+    No(int dado){
+        this.v=dado;
+        this.prox=null;
+    }
+}
+
+
+
+
+/*
+ * 
+ * int hash(int chave, int tamanho) {
+return chave % tamanho;
+}
+ * 
+ */
+
+
+
+public class ht_stringw{
+    //impl
+}
