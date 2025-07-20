fabbce3cde0d9f39f30ad735761200ff6a021440
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-07-20 22:39:37 +0000

    add class chain_sdg

--- minilist-ht/chain_sdg.java
+++ minilist-ht/chain_sdg.java
@@ -1,6 +1,6 @@
 //Implemente a remoção com encadeamento e sondagem
 //tabela hash 5 primeiros digitos CPF e dois primeiros nomes
-public class No{
+class No{
     int val;
     No prox;
 
@@ -10,7 +10,7 @@ public class No{
     }
 }
 
-public class Chain{
+class Chain{
     private No[] table;
     private int size;
     //tabela com hashs de tipo No
@@ -73,21 +73,22 @@ public class Chain{
         No atual=table[index];
 
         while(atual!=null){
-            if(atual.val=v){
+            if(atual.val==v){
                 System.out.println("Valor encontrado! "+v+"\n");
                 return true;
             }else{
                 atual=atual.prox;
             }
-            return false;
+           
         }
+         return false;
     }
 
 
 
 }
 
-public class Sdg{
+class Sdg{
     private No[] table;
     private int size;
 
@@ -114,44 +115,30 @@ public class Sdg{
         
         return false;
     }
-    /*
-     * 
-     * public boolean remove(int v){
-        int index=hash(v);
-        No atual=table[index];
-        No ant=null;
 
-        while(atual!=null) {
-            if(atual.val==v){
-                if(ant==null) {
-                    table[index]=atual.prox;
-                }else{
-                    ant.prox=atual.prox;
-                }
-                size--;
-                System.out.println("Valor removido da tabela: "+v+"\n");
-                return true;
+     public void remove(int v){
+        int index=hash(v);
+        if(table[index].val==v){
+            table[index]=null; 
+        }//pesquisa apenas linear pelo elemento
+        System.out.println("Elemento "+v+" não encontrado\n");
+     }
+     //+ em: https://abre.ai/lprobing-quora-rmv 
+
+
+     public int tam(){
+        return size;
+     }
+
+     public void showSdg(){
+        //int index=hash(v);
+        for(int i=0;i<table.length;i++){
+            if(table[i]!=null){
+                System.out.println("Index "+ i +": "+table[i].val);
+            }else{
+                System.out.println("Index "+ i +": "+null);
             }
-            ant=atual;
-            atual=atual.prox;
         }
-        return false;
-    }
-     * 
-     */
-
-     public booblean remove(int v){
-        int index=hash(v);
-        int original_value=index;
-
-        do{
-            if(table[index]!=null && table[index].val==v){
-                    //table[index]=table.prox; o que viria aqui??
-                else{
-                     table[index]=table.prox;
-                }
-            
-        }while()
      }
 
 }
@@ -162,8 +149,25 @@ public class chain_sdg {
     public static void main(String[] args){
         System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+       Sdg l1= new Sdg(10);
+       Chain l2= new Chain(10);
+
+       /*    problema de inserção aqui!!!!
+       l1.insert(5);
+       l1.insert(19);
+       l1.insert(37);
+       l1.insert(18);
+       l1.insert(55);
+       l1.insert(55);
+       l1.insert(19);
+       l1.insert(370);
+       l1.insert(17);
+       l1.insert(56);
+        */
+
+       l1.showSdg();
+
+
+
     }
 }
