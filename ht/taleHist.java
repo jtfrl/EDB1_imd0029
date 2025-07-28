3fce30a9c6d0535f08624c03f6731fae308d268a
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-07-28 02:34:31 +0000

    add new impl read file, get all words

--- ht/taleHist.java
+++ ht/taleHist.java
@@ -1,7 +1,5 @@
 //q. 1 - Exercício 3 | Elimine as palavras repetidas de tale.txt. 
 //Depois, faça um histograma como o mostrado acima mas usando M = 100.
-
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
@@ -9,7 +7,12 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
-
+import java.io.BufferedReader; //uso do buffer
+import java.io.FileReader; //leitor de arquivos
+import java.io.IOException;
+import java.util.ArrayList; //!!! isso pode atrapalhar no uso de HT? 
+import java.util.Set;
+import java.util.HashSet;
 
 
 public class GHist extends Application{
@@ -34,18 +37,56 @@ public class GHist extends Application{
         Scene sceneDisplay=new Scene(barChart, 1000, 1000);
         stage.setScene(scene);
         stage.show();
+          
+    }
+}
+
+public class fileTxt{
+    String[] txt;
     
-        
-        
+
+    fileTxt(String[] txt){
+        this.txt=txt;
+    }
+    
+    public void readFile(String filePath){
+        ArrayList<String> lines=new Arraylist<>();
+        try(BufferedReader br=new BufferedReader(new FileReader(filePath))){
+            String line;
+            while((line=br.readLine())!=null) lines.add(line);
+
+            this.txt=lines.toArray(new String[0]); //atributo toma os dados lidos 
+        }catch(IOException e){
+            System.err.println("Falha na leitura do arquivo\n"+filePath);
+            e.printStackTrace();
+        }
+
+    }
+
+    public String[] del_RepWords(String[] txt){
+        //String[] allWordsThatMatter;
+        long strLen=txt.length();
+
+        Set<String> uniqueWords=new HashSet<>();
+        //Set já lida com o caso de haver múltiplos dados iguais
+        //apenas um deles entra dentro container acima
+
+        for(long i=0; i<strLen;++i){
+            uniqueWords.add(txt[i]);
+        }
+       /* 
+            if(txt[i]!=txt[i+1]){
+                uniqueWords.add(txt[i]);
+            }
+        não lida bem com casos como [...he and he...]
+            */
+       
+        return uniqueWords.toArray(new String[0]);
     }
 }
 
 
-public class delRepWords{
-    public String[] del_RepWords(/* arq txt aqui*/){
-        //ver implementação para countMaxRepeats em DNAprofiler
-    }
-}
+
 
 public class taleHist{
     public static void Main (String [] args){
